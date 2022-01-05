ee5338c7b324a8825536c9dce425478d475dc794
FelixPalacios117 <00055419@uca.edu.sv>
2022-01-05 17:41:11 -0600

    nuevo

--- app_react/src/App.js
+++ app_react/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
 function App() {
   return (
-    <div className="App">
-      si funciona
+    <div className=" flex flex-row h-screen bg-black">
+      <div className=' bg-blue-600 h-80 basis-1/3'> 
+      k
+      </div>
+      <div className=' bg-black h-80 text-slate-50 basis-1/3'>
+        <p>jaja</p>
+      </div>
     </div>
   );
 }
