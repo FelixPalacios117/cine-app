ce4cb16811029faad4e9bc7e43a2b5d45fe25f21
Daniel-29 <daniel.alex.barrera@hotmail.com>
2022-01-10 17:24:05 -0600

    Co-authored-by: FelixPalacios117 <FelixPalacios117@users.noreply.github.com>

--- cine_backend/src/routes/index.js
+++ cine_backend/src/routes/index.js
@@ -1,42 +1,40 @@
 const customerController = require('../controllers/customerController');
 const multer = require('multer');
 const upload = multer({ dest: './images' })
-const shortid=require('shortid')
+const shortid = require('shortid')
 
 const routes = async (server) => {
-    // new-user
-    // delete-user
-    // update-user
-    // get-user
-    // list-user 
+  // new-user
+  // delete-user
+  // update-user
+  // get-user
+  // list-user 
 
-    //agregar un cliente
-    var storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, './uploads')//destino
-        },
-        filename: function (req, file, cb) {
-         // obtener la extensión del archivo
-        const extension = file.mimetype.split('/')[1];
-        // generar ID para ponerlo como nombre de imagen y no se repitan
-        cb(null, `${shortid.generate()}.${extension}`);
-            }
-      })
-      var upload = multer({ storage: storage })//necesario para acceder a carpetas
-    server.post('/new-customer', upload.single('image'), function (req, res, next) { 
-        next()
-      },customerController.add) 
-    //leer un cliente
-    server.get('/get-customer/:id', customerController.showById);
-    //mostrar clientes
-    server.get('/list-customers', customerController.showAll);
-    //actualizar un cliente
-    server.put('/update-customer/:id', upload.single('image'), function (req, res, next) { 
-        next()
-      },customerController.update);
-    //eliminar un cliente
-    server.delete('/delete-customer', customerController.delete);
-    //return router;
+  //agregar un cliente
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')//destino
+    },
+    filename: function (req, file, cb) {
+      // obtener la extensión del archivo
+      const extension = file.mimetype.split('/')[1];
+      // generar ID para ponerlo como nombre de imagen y no se repitan
+      cb(null, `${shortid.generate()}.${extension}`);
+    }
+  })
+  var upload = multer({ storage: storage })//necesario para acceder a carpetas
+  server.post('/new-customer', upload.single('image'), customerController.add)
+  //leer un cliente
+  server.get('/get-customer/:id', customerController.showById);
+  //mostrar clientes
+  server.get('/list-customers', customerController.showAll);
+  //actualizar un cliente
+  server.put('/update-customer/:id', upload.single('image'), function (req, res, next) {
+    next()
+  }, customerController.update);
+  //eliminar un cliente
+  server.delete('/delete-customer', customerController.delete);
+  //return router;
 }
 
 module.exports = { routes }
